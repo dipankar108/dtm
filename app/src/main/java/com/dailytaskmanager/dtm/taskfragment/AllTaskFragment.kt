2f3c4fbd1d2484420fd22a82e50bff0aab1e8046
dipankar108 <nathbd10@gmail.com>
2022-05-01 21:20:13 +0600

    Create new package for taskfragment

--- app/src/main/java/com/dailytaskmanager/dtm/taskfragment/AllTaskFragment.kt
+++ app/src/main/java/com/dailytaskmanager/dtm/taskfragment/AllTaskFragment.kt
@@ -1,10 +1,11 @@
-package com.dailytaskmanager.dtm
+package com.dailytaskmanager.dtm.taskfragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dailytaskmanager.dtm.R
 
 class AllTaskFragment : Fragment() {
     override fun onCreateView(
--- app/src/main/java/com/dailytaskmanager/dtm/taskfragment/RunningTaskFragment.kt
+++ app/src/main/java/com/dailytaskmanager/dtm/taskfragment/RunningTaskFragment.kt
@@ -1,17 +1,18 @@
-package com.dailytaskmanager.dtm
+package com.dailytaskmanager.dtm.taskfragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dailytaskmanager.dtm.R
 
-class UpcomingTaskFragment : Fragment() {
+class RunningTaskFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming_task, container, false)
+        return inflater.inflate(R.layout.fragment_running_task, container, false)
     }
 }
