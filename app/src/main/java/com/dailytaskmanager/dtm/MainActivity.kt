a00634736f1fbec01197c42856bd59c79231e054
dipankar108 <nathbd10@gmail.com>
2022-05-01 21:14:23 +0600

    Lots of design added to the apps

--- app/src/main/java/com/dailytaskmanager/dtm/MainActivity.kt
+++ app/src/main/java/com/dailytaskmanager/dtm/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.dailytaskmanager.dtm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val actionbar = supportActionBar
+        val colorDrawable = ColorDrawable(Color.parseColor("#17EBDE"))
+        actionbar?.setBackgroundDrawable(colorDrawable)
         setContentView(R.layout.activity_main)
     }
 }
