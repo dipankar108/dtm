56a0b6daca8615836a82859b032f31ead6f97886
dipankar108 <nathbd10@gmail.com>
2022-05-01 20:00:49 +0600

    Initial commit

---
+++ app/src/main/java/com/dailytaskmanager/dtm/SpalshActivity.kt
@@ -0,0 +1,11 @@
+package com.dailytaskmanager.dtm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpalshActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalsh)
+    }
+}
